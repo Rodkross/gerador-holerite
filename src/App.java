c6513cca354b95826b025aaea7c21fe742706864
Rodrigo Souza <eudevrodrigo@gmail.com>
2025-03-20 00:43:17 -0300

    k

--- src/App.java
+++ src/App.java
@@ -3,9 +3,7 @@ import departamentos.OperadoraCaixa;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        // nome - empresa - salario base - salario familia - dias trabalhados - n filhos
-        // - n feriados (vencimentos)
-        // desconto vale - desconto INSS - desconto convenio - numero faltas
+        
         Balconista balconista1 = new Balconista("João Alcebiades Benevides Da Silva", "Drogaria Nossa Farma ltda",
                 1643.23,
                 30, 1, 0, 20, 150, 0);
