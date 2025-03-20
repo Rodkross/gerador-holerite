c7bb6616196cc48a9bc5b42573e50f26b183d48b
Rodrigo Souza <eudevrodrigo@gmail.com>
2025-03-19 22:19:19 -0300

    desenvolvendo o perfil de caixa

--- src/App.java
+++ src/App.java
@@ -1,4 +1,5 @@
 import departamentos.Balconista;
+import departamentos.OperadoraCaixa;
 
 public class App {
     public static void main(String[] args) throws Exception {
@@ -7,8 +8,12 @@ public class App {
         Balconista balconista1 = new Balconista("João Alcebiades Benevides Da Silva", "Drogaria Nossa Farma ltda", 1643.23, 65.00,
                 30, 0, 1,20,150,1);
 
+        OperadoraCaixa operadorCaixa1 = new OperadoraCaixa("Elizandra Muniz", "Drogaria Nossa Farma ltda", 1643.23, 65, 30, 1, 0, 0, 0, 0);        
+
         
         balconista1.exibirInformacoes();
+        System.out.println("\n----------------------------------------\n");
+        operadorCaixa1.exibirInformacoes();
         
 
     }
---
+++ src/departamentos/OperadoraCaixa.java
@@ -0,0 +1,47 @@
+package departamentos;
+
+public class OperadoraCaixa extends Pessoa {
+    private double quebraDeCaixa;
+
+    public OperadoraCaixa(String nome, String empresa, Double salarioBase, double salarioFamilia,
+            int diasTrabalhados, int numeroDeFilhos, int numeroDeFeriados, double descontoVale, double descontoConvenio,
+            int descontoFaltas) {
+        super(nome, empresa, salarioBase, salarioFamilia, diasTrabalhados, numeroDeFilhos, numeroDeFeriados,
+                descontoVale, descontoConvenio, descontoFaltas);
+                this.quebraDeCaixa = quebraDeCaixa;
+    }
+
+    public double calcularQuebraCaixa() {
+        double valorQuebraDeCaixa = this.calcularDiasTrabalhados() * 10 /100;
+        return valorQuebraDeCaixa;
+    }
+
+    public double calcularVencimentos() {
+        double vencimentosValor = this.calcularDiasTrabalhados() + this.calcularSalarioFamilia() + this.calcularFeriado() + this.calcularQuebraCaixa();
+        return vencimentosValor;
+    }
+
+    public void exibirInformacoes() {
+        System.out.println("\nNome: " + this.getNome());
+        System.out.println("Empresa: " + this.getEmpresa());
+        System.out.printf("\n");
+        System.out.printf("\nDias trabalhados: %.2f", this.calcularDiasTrabalhados());
+        System.out.printf("\nSalário Família: %.2f", this.calcularSalarioFamilia());
+        System.out.printf("\nFeriado: %.2f", this.calcularFeriado());
+        System.out.printf("\nQuebra de caixa: %.2f", this.calcularQuebraCaixa());
+        System.out.printf("\n");
+        System.out.printf("\nVencimentos .................. R$%.2f", calcularVencimentos());
+        System.out.printf("\n");
+        System.out.printf("\nVales: %.2f", this.getDescontoVale()); //corrigir calculo
+        System.out.printf("\nINSS: %.2f", this.calcularINSS());
+        System.out.printf("\nConvênio funcionário: %.2f", getDescontoConvenio());
+        System.out.printf("\nFaltas: %.2f", this.calcularFaltas());
+        System.out.printf("\n");
+        System.out.printf("\nDescontos ......................... R$%.2f", calcularDescontos());
+        System.out.printf("\n");
+        System.out.println("\n-----------------------------------");
+        System.out.printf("Salário Líquido: R$%.2f ", this.calcularSalarioLiquido());
+        System.out.println("\n-----------------------------------");
+    }
+         
+}
